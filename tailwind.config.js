ea1ea29e6693c2285c02f694a2fb02d9580d56fa
Z1ZA <sayaabdaziz1@gmail.com>
2023-09-29 20:46:52 +0700

    add mobile responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html"],
+  content: ["index.html", "responsive.html"],
   theme: {
+    screens: {
+      sm: "360px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
     extend: {},
   },
   plugins: [],
