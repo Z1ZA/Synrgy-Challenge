72b69d22da9f78f04e312c8eb1f54e9da3fcb348
Z1ZA <sayaabdaziz1@gmail.com>
2023-10-12 17:34:38 +0700

    add challenge_3

---
+++ scripts/sortCarByYearDecendingly.js
@@ -0,0 +1,17 @@
+function sortCarByYearDescendingly(cars) {
+  // Sangat dianjurkan untuk console.log semua hal hehe
+  console.log(cars);
+
+  // Clone array untuk menghindari side-effect
+  // Apa itu side effect?
+  const result = [...cars];
+
+  // Tulis code-mu disini
+  function compareByYear(a, b) {
+    return b.year - a.year;
+  }
+  result.sort(compareByYear);
+
+  // Rubah code ini dengan array hasil sorting secara descending
+  return result;
+}
