b1a07126b68710747b339ec8dbd6efc8b3d755d1
Z1ZA <sayaabdaziz1@gmail.com>
2024-02-13 11:10:05 +0700

    feat: add challenge 6

---
+++ knexfile.ts
@@ -0,0 +1,53 @@
+import { Knex } from "knex";
+
+const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: "postgresql",
+    connection: {
+      database: "binar-car-rental",
+      user: "postgres",
+      password: "postgres",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "binar-car-rental",
+      user: "postgres",
+      password: "postgres",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "binar-car-rental",
+      user: "postgres",
+      password: "postgres",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};
+
+module.exports = config;
